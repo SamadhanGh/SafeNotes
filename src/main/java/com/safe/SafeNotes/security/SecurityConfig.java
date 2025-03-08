05dff2f70351d37ae18c34bf943d8c540cc8570f
Samadhan Ghorpade <sdghorpade2003@gmail.com>
2025-03-09 03:58:52 +0530

    Defining user model and Repository

--- src/main/java/com/safe/SafeNotes/security/SecurityConfig.java
+++ src/main/java/com/safe/SafeNotes/security/SecurityConfig.java
@@ -4,8 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -13,18 +20,61 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/contact").permitAll()
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("/admin/**").denyAll()
-                .anyRequest().authenticated());
-//        http.formLogin(withDefaults());
-
-        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests((requests)
+                -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+       JdbcUserDetailsManager manager =
+               new JdbcUserDetailsManager(dataSource);
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
+    }
+
 }
+
+
+
+
+
+//public class SecurityConfig {
+//
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) -> requests
+//                .requestMatchers("/contact").permitAll()
+//                .requestMatchers("/public/**").permitAll()
+//                .requestMatchers("/admin/**").denyAll()
+//                .anyRequest().authenticated());
+////        http.formLogin(withDefaults())
+//
+//        //FOR disabling the CSRF
+//        http.csrf(csrf -> csrf.disable());
+//
+//        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.httpBasic(withDefaults());
+//        return http.build();
+//    }
+//}

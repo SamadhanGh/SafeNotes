394e648b6e098170127370048d2a1c2097362c4f
Samadhan Ghorpade <sdghorpade2003@gmail.com>
2025-03-15 17:53:01 +0530

    implementing the login and register functionality

---
+++ src/main/java/com/safe/SafeNotes/security/response/MessageResponse.java
@@ -0,0 +1,17 @@
+
+// MessageResponse.java
+package com.safe.SafeNotes.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+}
